25043f8580e1c21cd73a6f2c11e52b719ab56f6f
P11co2 <gh05tsmic@gmail.com>
2022-05-20 10:18:26 +0900

    0x01 updated notes

--- 0x01/문제1.cpp
+++ 0x01/문제1.cpp
@@ -3,6 +3,12 @@
 
 using namespace std;
 
+/* Key points:
+1. Time-space complexity and O(N) notation
+2. Data types and their limitations (overflow) - 정수형, 실수형 (integer, float type)
+*/
+
+// 1. 시간-공간 복잡도 연습
 // O(N)
 int func1(int N) {
     int x = 0;
@@ -58,6 +64,39 @@ int main() {
     // int b4 = func4(5);
     // cout << a4 << endl;
     // cout << b4 << endl;
+
+    // 정수형, 실수형 종류 (overflow)
+    /*  Integer Data type,      Bytes,          Range
+    signed char                 (1)             -(2**7)     to 2**7 - 1
+    unsigned char               (1)             -(2**8)     to 2**8 - 1
+    short int (signed)          (2)             -(2**15)    to 2**15 - 1
+    unsigned short int          (2)             0           to 2**16 - 1
+    unsigned int                (4)             0           to 2**32 - 1        
+    int (signed)                (4)             -(2**31)    to 2**31 - 1
+    unsigned long int           (4)             0           to 2**32 - 1
+    long int (signed)           (4)             -(2**31)    to 2**31 - 1           
+    long long int (signed)      (8)             -(2**63)    to (2**63) - 1
+    unsigned long long int      (8)             0           to 2**64 - 1
+        Float Data type,        sign,       exponent,       fraction field
+    float                       1 bit       8 bits          23 bits
+    double                      1 bit       11 bits         52 bits
+    long double                 1 bit
+    */
+
+    // Adding 1 sends y to the negative space (for signed types)
+    // short y = pow(2, 15) - 1;
+    // cout << y << "\n";
+    // y += 1;
+    // cout << y << "\n";
+
+    // Adding 1 sends x to 0 (for unsigned types)
+    // unsigned int x = pow(2, 32) - 1;
+    // cout << x << "\n";
+    // x += 1;
+    // cout << x << "\n";
+    // x = -2;
+    // cout << x << "\n";
+
     return 0;
 }
 
