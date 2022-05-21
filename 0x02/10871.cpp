e54101d0b2182ef83fbfc76f2bba9dae7ce52491
P11co2 <gh05tsmic@gmail.com>
2022-05-21 22:06:27 +0900

    update

---
+++ 0x02/10871.cpp
@@ -0,0 +1,19 @@
+// 연습 문제 	10871 	X보다 작은 수
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // Alternately, replace "false" or "nullptr" with "0"
+    // ios::sync_with_stdio(0);
+    // cin.tie(0);
+    string input_info;
+    getline(cin, input_info);
+    cout << input_info << "\n";
+    // string input;
+    // getline(cin, input);
+
+    return 0;
+}
